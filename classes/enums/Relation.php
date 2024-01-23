31c237cd72dfa08c50b40956d10b283fa674892a
dimti <demidov@dimti.ru>
2024-01-23 12:46:32 +0300

    + polymorphic relation types support

--- classes/enums/Relation.php
+++ classes/enums/Relation.php
@@ -2,15 +2,16 @@
 
 enum Relation: string
 {
-    case RELATION_BELONGS_TO_MANY = 'belongsToMany';
-
-    case RELATION_BELONGS_TO = 'belongsTo';
-
+    case RELATION_HAS_ONE = 'hasOne';
     case RELATION_HAS_MANY = 'hasMany';
 
-    case RELATION_HAS_ONE = 'hasOne';
+    case RELATION_BELONGS_TO = 'belongsTo';
+    case RELATION_BELONGS_TO_MANY = 'belongsToMany';
 
     case RELATION_ATTACH_ONE = 'attachOne';
-
     case RELATION_ATTACH_MANY = 'attachMany';
+
+    case RELATION_MORPH_TO = 'morphTo';
+    case RELATION_MORPH_ONE = 'morphOne';
+    case RELATION_MORPH_MANY = 'morphMany';
 }

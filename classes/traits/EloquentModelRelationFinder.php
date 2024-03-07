e85d9d4cb7e8c849e71024584ccde446047b3392
dimti <demidov@dimti.ru>
2024-03-07 11:12:56 +0300

    + get pivot table and fields

--- classes/traits/EloquentModelRelationFinder.php
+++ classes/traits/EloquentModelRelationFinder.php
@@ -1,5 +1,6 @@
 <?php namespace Octobro\API\Classes\traits;
 
+use Illuminate\Database\Eloquent\Concerns\HasRelationships;
 use October\Rain\Database\Model;
 use Octobro\API\Classes\enums\Relation;
 use Octobro\API\Classes\Exceptions\OctobroApiException;
@@ -23,6 +24,20 @@ trait EloquentModelRelationFinder
     public function hasRelation(Model|\Winter\Storm\Database\Model|\Illuminate\Database\Eloquent\Model $parentModel, string $mayBeRelation): bool
     {
         return $this->getRelationType($parentModel, $mayBeRelation) !== null;
+    }
+
+    /**
+     * @throws OctobroApiException
+     */
+    public function checkRelationDefinition(Model|\Winter\Storm\Database\Model|\Illuminate\Database\Eloquent\Model $parentModel, string $mayBeRelation): void
+    {
+        if (!$this->getRelationDefinition($parentModel, $mayBeRelation)) {
+            throw new OctobroApiException(sprintf(
+                'Unable to get relation definition for: %s.%s',
+                get_class($parentModel),
+                $mayBeRelation
+            ));
+        }
     }
 
     /**
@@ -55,5 +70,53 @@ trait EloquentModelRelationFinder
         $relationClassName = $this->getRelationModelClassName($parentModel, $mayBeRelation);
 
         return new $relationClassName;
+    }
+
+    /**
+     * @throws OctobroApiException
+     */
+    public function hasPivotFields(Model|\Winter\Storm\Database\Model|\Illuminate\Database\Eloquent\Model $parentModel, string $mayBeRelation): bool
+    {
+        $this->checkRelationDefinition($parentModel, $mayBeRelation);
+
+        $relationDefinition = $this->getRelationDefinition($parentModel, $mayBeRelation);
+
+        return is_array($relationDefinition) && array_key_exists('pivot', $relationDefinition);
+    }
+
+    /**
+     * @throws OctobroApiException
+     */
+    public function getPivotFields(Model|\Winter\Storm\Database\Model|\Illuminate\Database\Eloquent\Model $parentModel, string $mayBeRelation)
+    {
+        $this->checkRelationDefinition($parentModel, $mayBeRelation);
+
+        $relationDefinition = $this->getRelationDefinition($parentModel, $mayBeRelation);
+
+        return $relationDefinition['pivot'];
+    }
+
+    /**
+     * @throws OctobroApiException
+     */
+    public function getPivotTable(Model|\Winter\Storm\Database\Model|\Illuminate\Database\Eloquent\Model $parentModel, string $mayBeRelation)
+    {
+        if ($relationDefinition = $this->getRelationDefinition($parentModel, $mayBeRelation)) {
+            if (is_array($relationDefinition) && array_key_exists('table', $relationDefinition)) {
+                return $relationDefinition['table'];
+            } else {
+                /**
+                 * @see \Illuminate\Database\Eloquent\Model::getTable
+                 * @see HasRelationships::joiningTable
+                 */
+                return $parentModel->joiningTable($this->getRelationModel($parentModel, $mayBeRelation));
+            }
+        }
+
+        throw new OctobroApiException(sprintf(
+            'Unable to get pivot table for: %s.%s',
+            get_class($parentModel),
+            $mayBeRelation
+        ));
     }
 }

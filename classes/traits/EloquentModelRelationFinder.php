1a64ab11a1ec159cd40a160ee2db648aff827a1a
dimti <demidov@dimti.ru>
2024-03-13 19:11:24 +0300

    * use reflection for detect relations in models

--- classes/traits/EloquentModelRelationFinder.php
+++ classes/traits/EloquentModelRelationFinder.php
@@ -1,35 +1,72 @@
 <?php namespace Octobro\API\Classes\traits;
 
 use Illuminate\Database\Eloquent\Concerns\HasRelationships;
-use October\Rain\Database\Model;
+use Illuminate\Database\Eloquent\Model;
 use Octobro\API\Classes\enums\Relation;
 use Octobro\API\Classes\Exceptions\OctobroApiException;
+use ReflectionClass;
+use ReflectionException;
 
 trait EloquentModelRelationFinder
 {
-    public function getRelationType(Model|\Winter\Storm\Database\Model|\Illuminate\Database\Eloquent\Model $parentModel, string $mayBeRelation): ?Relation
+    /**
+     * @throws ReflectionException
+     */
+    private function getReflectionClassOfModel(Model|string $parentModel): ReflectionClass
     {
-        $relationTypes = collect(Relation::cases());
-
-        return $relationTypes->filter(fn($relationTypeName) => array_key_exists($mayBeRelation, $parentModel->{$relationTypeName->value}))->first();
+        return new ReflectionClass(
+            is_string($parentModel) ? $parentModel : get_class($parentModel)
+        );
     }
 
-    public function getRelationDefinition(Model|\Winter\Storm\Database\Model|\Illuminate\Database\Eloquent\Model $parentModel, string $mayBeRelation): array|string|null
+    /**
+     * @throws ReflectionException
+     */
+    private function getMayBeRelationDefinitionByReflect(ReflectionClass $reflect, Relation $relationType, string $mayBeRelation): array|string
+    {
+        return $reflect->getProperty($relationType->value)?->getDefaultValue()[$mayBeRelation];
+    }
+
+    /**
+     * @throws ReflectionException
+     */
+    public function getRelationType(Model|string $parentModel, string $mayBeRelation): ?Relation
+    {
+        $reflect = $this->getReflectionClassOfModel($parentModel);
+
+        return collect(Relation::cases())->filter(
+            fn ($relationTypeName) => array_key_exists(
+                $mayBeRelation,
+                ($reflect->getProperty($relationTypeName->value)?->getDefaultValue() ?? [])
+            )
+        )->first();
+    }
+
+    /**
+     * @throws ReflectionException
+     */
+    public function getRelationDefinition(Model|string $parentModel, string $mayBeRelation): array|string|null
     {
         $relationType = $this->getRelationType($parentModel, $mayBeRelation);
 
-        return $relationType ? $parentModel->{$relationType->value}[$mayBeRelation] : null;
+        $reflect = $this->getReflectionClassOfModel($parentModel);
+
+        return $relationType ? $this->getMayBeRelationDefinitionByReflect($reflect, $relationType, $mayBeRelation) : null;
     }
 
-    public function hasRelation(Model|\Winter\Storm\Database\Model|\Illuminate\Database\Eloquent\Model $parentModel, string $mayBeRelation): bool
+    /**
+     * @throws ReflectionException
+     */
+    public function hasRelation(Model|string $parentModel, string $mayBeRelation): bool
     {
         return $this->getRelationType($parentModel, $mayBeRelation) !== null;
     }
 
     /**
      * @throws OctobroApiException
+     * @throws ReflectionException
      */
-    public function checkRelationDefinition(Model|\Winter\Storm\Database\Model|\Illuminate\Database\Eloquent\Model $parentModel, string $mayBeRelation): void
+    public function checkRelationDefinition(Model|string $parentModel, string $mayBeRelation): void
     {
         if (!$this->getRelationDefinition($parentModel, $mayBeRelation)) {
             throw new OctobroApiException(sprintf(
@@ -42,8 +79,9 @@ trait EloquentModelRelationFinder
 
     /**
      * @throws OctobroApiException
+     * @throws ReflectionException
      */
-    public function getRelationModelClassName(Model|\Winter\Storm\Database\Model|\Illuminate\Database\Eloquent\Model $parentModel, string $mayBeRelation): string
+    public function getRelationModelClassName(Model|string $parentModel, string $mayBeRelation): string
     {
         if ($relationDefinition = $this->getRelationDefinition($parentModel, $mayBeRelation)) {
             if (is_array($relationDefinition)) {
@@ -64,8 +102,9 @@ trait EloquentModelRelationFinder
 
     /**
      * @throws OctobroApiException
+     * @throws ReflectionException
      */
-    public function getRelationModel(Model|\Winter\Storm\Database\Model|\Illuminate\Database\Eloquent\Model $parentModel, string $mayBeRelation): Model|\Winter\Storm\Database\Model|\Illuminate\Database\Eloquent\Model
+    public function getRelationModel(Model|string $parentModel, string $mayBeRelation): Model
     {
         $relationClassName = $this->getRelationModelClassName($parentModel, $mayBeRelation);
 
@@ -74,8 +113,9 @@ trait EloquentModelRelationFinder
 
     /**
      * @throws OctobroApiException
+     * @throws ReflectionException
      */
-    public function hasPivotFields(Model|\Winter\Storm\Database\Model|\Illuminate\Database\Eloquent\Model $parentModel, string $mayBeRelation): bool
+    public function hasPivotFields(Model|string $parentModel, string $mayBeRelation): bool
     {
         $this->checkRelationDefinition($parentModel, $mayBeRelation);
 
@@ -86,8 +126,9 @@ trait EloquentModelRelationFinder
 
     /**
      * @throws OctobroApiException
+     * @throws ReflectionException
      */
-    public function getPivotFields(Model|\Winter\Storm\Database\Model|\Illuminate\Database\Eloquent\Model $parentModel, string $mayBeRelation)
+    public function getPivotFields(Model|string $parentModel, string $mayBeRelation)
     {
         $this->checkRelationDefinition($parentModel, $mayBeRelation);
 
@@ -98,15 +139,16 @@ trait EloquentModelRelationFinder
 
     /**
      * @throws OctobroApiException
+     * @throws ReflectionException
      */
-    public function getPivotTable(Model|\Winter\Storm\Database\Model|\Illuminate\Database\Eloquent\Model $parentModel, string $mayBeRelation)
+    public function getPivotTable(Model|string $parentModel, string $mayBeRelation)
     {
         if ($relationDefinition = $this->getRelationDefinition($parentModel, $mayBeRelation)) {
             if (is_array($relationDefinition) && array_key_exists('table', $relationDefinition)) {
                 return $relationDefinition['table'];
             } else {
                 /**
-                 * @see \Illuminate\Database\Eloquent\Model::getTable
+                 * @see Model::getTable
                  * @see HasRelationships::joiningTable
                  */
                 return $parentModel->joiningTable($this->getRelationModel($parentModel, $mayBeRelation));
